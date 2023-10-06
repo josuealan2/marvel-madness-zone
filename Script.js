9ef54398ebf2e9b61a54b829f7224f34e01276a3
Wxxlfe <142287140+Wxxlfe@users.noreply.github.com>
2023-10-05 18:37:42 -0700

    marvel intro java script and html

--- Script.js
+++ Script.js
@@ -1,14 +1,33 @@
-var YoutubeKey = 'AIzaSyC0PpzjRDdgeE-NDNEE_kjaIZ5LsdLQg4c';
+var apiKey = 'AIzaSyC0PpzjRDdgeE-NDNEE_kjaIZ5LsdLQg4c';
+var videoId = 'os2C0TdDphc';
 
-function YoutubePlayer(){
-  var PLayer = new YoutubeKey.Player('player', {
-    height: 500,
-    width: 1519,
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: 550,
+    width: 1904,
     videoId: 'os2C0TdDphc',
     playerVars: {
       'autoplay': 0,
       'controls': 1,
       'rel': 0,
+    },
+    events: {
+      'onReady': onPlayerReady,
     }
-  })
+  });
 }
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=os2C0TdDphc&key=AIzaSyC0PpzjRDdgeE-NDNEE_kjaIZ5LsdLQg4c")
+.then(Response => Response.json())
+.then(data => {
+  console.log(data);
+})
+.catch(error => {
+  console.error('Error fetching data from Youtube API', error);
+});
